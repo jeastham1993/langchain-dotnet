8c337950505c7b98eb90d50a1693f3393a343fbd
James Eastham <dev@jameseastham.co.uk>
2023-05-05 22:30:07 +0100

    Add examples and repo files

---
+++ examples/LangChain.NET.HuggingFace/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using LangChain.NET.LLMS.HuggingFace;
+
+Console.WriteLine("What is your Hugging Face API Key?");
+
+var secondModel = new HuggingFace(new HuggingFaceConfiguration()
+{
+    ApiKey = Console.ReadLine(),
+    ModelName = "gpt2",
+    Temperature = 0.9M
+});
+
+var hfResult = await secondModel.Call("The man walked his");
+
+Console.WriteLine(hfResult);
--- examples/LangChain.NET.OpenAISample/Program.cs
+++ examples/LangChain.NET.OpenAISample/Program.cs
@@ -11,18 +11,7 @@ var model = new OpenAI(new OpenAIConfiguration()
     MaxTokens = 1000,
 });
 
-Console.WriteLine("What is your Hugging Face API Key?");
-var secondModel = new HuggingFace(new HuggingFaceConfiguration()
-{
-    ApiKey = Console.ReadLine(),
-    ModelName = "gpt2",
-    Temperature = 0.9M
-});
-
 var result = await model.Call("Write me a limerick about generative AI?");
 
 Console.WriteLine(result);
 
-var hfResult = await secondModel.Call("The man walked his");
-
-Console.WriteLine(hfResult);
