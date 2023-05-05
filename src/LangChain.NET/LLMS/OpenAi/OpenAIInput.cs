9ec860bf9c3f5b185fcb41cb77f4ce23a21acf98
jamesuk <jamesuk@amazon.com>
2023-05-05 19:43:52 +0100

    Add gitignore

---
+++ src/LangChain.NET/LLMS/OpenAi/OpenAIInput.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------------------
+// <copyright file="OpenAIInput.cs" company="BP p.l.c.">
+// ---------------------------------------------------------------------------
+// Copyright 2023 BP p.l.c. All Rights Reserved.
+// Also protected by the Digital Millennium Copyright Act (DMCA) and
+// afforded all remedies allowed under 17 U.S.C. § 1203.
+// Proprietary and Confidential information of BP p.l.c.
+// Disclosure, Use, or Reproduction without the written authorization
+// of BP p.l.c. is prohibited.
+// ---------------------------------------------------------------------------
+// Author: Eastham, James
+// ---------------------------------------------------------------------------
+// </copyright>
+// ---------------------------------------------------------------------------
+
+namespace LangChain.NET.LLMS.OpenAi;
+
+public interface OpenAIInput
+{
+    
+}
