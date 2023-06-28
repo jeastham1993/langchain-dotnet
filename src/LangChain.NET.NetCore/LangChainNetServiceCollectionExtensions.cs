e4d3821b4a61fe9e570763e3bdbd4297f2efa952
Jakub Baranowski <jakub@curvestone.io>
2023-06-28 15:26:17 +0200

    Refactor to allow AddHttpClient, extension method for service collection and AspNetCore example

---
+++ src/LangChain.NET.NetCore/LangChainNetServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using LangChain.NET.LLMS.OpenAi;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LangChain.NET.NetCore;
+
+public static class LangChainNetServiceCollectionExtensions
+{
+    public static IServiceCollection AddOpenAi(
+        this IServiceCollection services)
+    {
+        if (services == null) throw new ArgumentNullException(nameof(services));
+
+        var optionsBuilder = services.AddOptions<OpenAiConfiguration>();
+        
+        optionsBuilder.BindConfiguration("OpenAi");
+        
+        services.AddHttpClient<OpenAi>(httpClient =>
+        {
+            httpClient.BaseAddress = new Uri("https://api.openai.com/v1");
+        });
+        
+        return services;
+    }
+}
