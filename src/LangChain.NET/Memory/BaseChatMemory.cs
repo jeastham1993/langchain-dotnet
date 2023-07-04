87bfd131a70eb9e142a7c6f47f376975bb35784d
Uros Biberdzic <b.urosh@gmail.com>
2023-07-04 21:35:30 +0200

    Revert "added initial clases for ConversationBufferMemory"
    This reverts commit 8baace69faffc524355d1d8b0fa750576a460133.

--- src/LangChain.NET/Memory/BaseChatMemory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LangChain.NET.Memory
-{
-    public abstract class BaseChatMemory : BaseMemory
-    {
-        public ChatMessageHistory chatHistory;
-
-        public abstract Task<MemoryVariables> loadMemoryVariables(InputValues values);
-    }
-}
