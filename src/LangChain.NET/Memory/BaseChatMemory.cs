8baace69faffc524355d1d8b0fa750576a460133
Uros Biberdzic <b.urosh@gmail.com>
2023-07-04 21:33:46 +0200

    added initial clases for ConversationBufferMemory

---
+++ src/LangChain.NET/Memory/BaseChatMemory.cs
@@ -0,0 +1,9 @@
+﻿namespace LangChain.NET.Memory
+{
+    public abstract class BaseChatMemory : BaseMemory
+    {
+        public ChatMessageHistory chatHistory;
+
+        public abstract Task<MemoryVariables> loadMemoryVariables(InputValues values);
+    }
+}
---
+++ src/LangChain.NET/Memory/BaseMemory.cs
@@ -0,0 +1,11 @@
+﻿namespace LangChain.NET.Memory
+{
+    public abstract class BaseMemory
+    {
+        public abstract string[] memoryKeys { get; }
+
+        public abstract Task<MemoryVariables> LoadMemoryVariables(InputValues values);
+
+        public abstract Task SaveContext(InputValues inputValues, OutputValues outputValues);
+    }
+}
